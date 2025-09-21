930319e27f45f0361ad3b604a80a86a92ecd935d
Lodya_zw74@top-academy.ru <Lodya_zw74@top-academy.ru>
2025-09-22 00:24:32 +0500

    Encoding

--- WinAPI/main.cpp
+++ WinAPI/main.cpp
@@ -1,4 +1,4 @@
-#include <Windows.h>
+п»ї#include <Windows.h>
 #include "resource.h"
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -8,7 +8,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR IpCmdLine, IN
 	/*MessageBox
 	(
 		NULL,
-		"Привет WinAPI! \nКак Ваше настроение?",
+		"РџСЂРёРІРµС‚ WinAPI! \nРљР°Рє Р’Р°С€Рµ РЅР°СЃС‚СЂРѕРµРЅРёРµ?",
 		"Hello",
 		MB_YESNOCANCEL | MB_ICONQUESTION | MB_HELP | MB_DEFBUTTON3 | MB_SYSTEMMODAL
 	);*/
@@ -22,7 +22,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-	case WM_INITDIALOG:		//Эта секция выполняется один раз, нужна для добавления элементов в окно диалога
+	case WM_INITDIALOG:		//Р­С‚Р° СЃРµРєС†РёСЏ РІС‹РїРѕР»РЅСЏРµС‚СЃСЏ РѕРґРёРЅ СЂР°Р·, РЅСѓР¶РЅР° РґР»СЏ РґРѕР±Р°РІР»РµРЅРёСЏ СЌР»РµРјРµРЅС‚РѕРІ РІ РѕРєРЅРѕ РґРёР°Р»РѕРіР°
 	{
 	HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
 	SetFocus(hEditLogin);
@@ -30,7 +30,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
 	}
 		break;
-	case WM_COMMAND:		//Обрабатывает команды с клавиатуры и мыши
+	case WM_COMMAND:		//РћР±СЂР°Р±Р°С‚С‹РІР°РµС‚ РєРѕРјР°РЅРґС‹ СЃ РєР»Р°РІРёР°С‚СѓСЂС‹ Рё РјС‹С€Рё
 		switch (LOWORD(wParam))
 		{
 		case IDC_BUTTON_COPY:
@@ -43,11 +43,11 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 		}
 		break;
-		case IDOK:		MessageBox(hwnd, "Была нажата кнопка OK", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDOK:		MessageBox(hwnd, "Р‘С‹Р»Р° РЅР°Р¶Р°С‚Р° РєРЅРѕРїРєР° OK", "Info", MB_OK | MB_ICONINFORMATION); break;
 		case IDCANCEL:	EndDialog(hwnd, 0); break;
 		}
 		break;
-	case WM_CLOSE:			//Закрывает окно
+	case WM_CLOSE:			//Р—Р°РєСЂС‹РІР°РµС‚ РѕРєРЅРѕ
 		EndDialog(hwnd, 0);
 		break;
 	}
