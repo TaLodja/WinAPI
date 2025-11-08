cb81fc2c9b33c7dfb8d6304b68d2b074e3179d47
Lodya_zw74@top-academy.ru <Lodya_zw74@top-academy.ru>
2025-11-08 14:27:49 +0500

    Resource-onlyDLL DONE

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <float.h>
 #include <stdio.h>
+//#include <iostream>
 #include "resource.h"
 
 CONST CHAR g_sz_CLASS_NAME[] = "Calc_SPU_411";
@@ -27,6 +28,7 @@ CONST INT g_i_OPERATIONS_START_X = g_i_BUTTON_START_X + (g_i_BUTTON_SIZE + g_i_I
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 VOID SetSkin(HWND hwnd, CONST CHAR SZ_SKIN[]);
+VOID SetSkinDLL(HWND hwnd, CONST CHAR SZ_SKIN[]);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -100,6 +102,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CREATE:
 	{
+		//Aloc();
+		//freopen("CONOUT", "w", stdout);
 		HWND hEdit = CreateWindowEx
 		(
 			NULL, "Edit", "0",
@@ -218,7 +222,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			GetModuleHandle(NULL),
 			NULL
 		);
-		SetSkin(hwnd, "metal_mistral");
+		SetSkinDLL(hwnd, "square_blue_class");
 	}
 	break;
 	case WM_COMMAND:
@@ -425,4 +429,26 @@ VOID SetSkin(HWND hwnd, CONST CHAR SZ_SKIN[])
 		);
 		SendMessage(GetDlgItem(hwnd, IDC_BUTTON_0+i), BM_SETIMAGE, 0, (LPARAM)hBitmap);
 	}
+}
+VOID SetSkinDLL(HWND hwnd, CONST CHAR SZ_SKIN[])
+{
+	HMODULE hSkin = LoadLibrary(SZ_SKIN);
+	for (int i = IDC_BUTTON_0; i <= IDC_BUTTON_EQUAL; i++)
+	{
+		//CHAR sz_filename[FILENAME_MAX] = {};
+		//sprintf(sz_filename, "ButtonsBMP\\%s\\button_%i.bmp", SZ_SKIN, i);
+		HBITMAP hBitmap = (HBITMAP)LoadImage
+		(
+			hSkin,
+			MAKEINTRESOURCE(i),
+			IMAGE_BITMAP,
+			i == IDC_BUTTON_0 ? g_i_BUTTON_DOUBLE_SIZE : g_i_BUTTON_SIZE,
+			i == IDC_BUTTON_EQUAL ? g_i_BUTTON_DOUBLE_SIZE : g_i_BUTTON_SIZE,
+			LR_SHARED
+		);
+		//DWORD dwLastCode = GetLastError();
+		//std::cout << dwLastCode << std::endl;
+		SendMessage(GetDlgItem(hwnd, i), BM_SETIMAGE, 0, (LPARAM)hBitmap);
+	}
+	FreeLibrary(hSkin);
 }
